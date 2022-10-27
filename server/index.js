43095985c3f56f150f81f46d5bd99cd9bce1b963
Canh <pcanhgm@gmail.com>
2022-10-27 21:49:08 +0700

    update db connect

--- server/index.js
+++ server/index.js
@@ -30,12 +30,12 @@ app.use(express.json())
 
 routes(app)
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
